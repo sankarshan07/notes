3f1b35710264e66c366030600107ebf657900ce6
sankarshan <sankarshandesai3107@gmail.com>
2025-03-03 11:24:32 +0530

    login page setup

---
+++ frontend/notes-app/src/components/Input/PasswordInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PasswordInput = ({value, onChange, placeholder}) => {
+  return (
+    <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3 outline-none'>
+      Password input
+    </div>
+  )
+}
+
+export default PasswordInput
+   
+
+// 14.59 resume
---
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import {Link} from 'react-router-dom'
+import PasswordInput from '../../components/Input/PasswordInput'
+
+const Login = () => {
+  return (
+   <> 
+   <Navbar/>
+   <div className='flex items-center justify-center mt-20'>
+    <div className='w-96 border rounded bg-white px-7 py-10' >
+        <form onSubmit={()=>{}}>
+            <h4 className='text-2xl mb-7'>Login</h4>
+            <input type="text" placeholder='Email' className='input-box'/>
+            <PasswordInput/>
+            <button type='submit' className='btn-primary'>Login</button>
+            <p className='text-sm text-center mt-4'>Not Registered yet?{""}
+                <Link to="/signUp" className="font-medium text-blue-500 underline">
+            Create an account
+            </Link></p>
+        </form>
+    </div>
+   </div>
+   </>
+   
+  )
+}
+
+export default Login
