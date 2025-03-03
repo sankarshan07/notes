07d957cd7a7619743cea84cdc8b74308cc39ab34
sankarshan <sankarshandesai3107@gmail.com>
2025-03-03 23:21:57 +0530

    password hook setup

--- frontend/notes-app/src/components/Input/PasswordInput.jsx
+++ frontend/notes-app/src/components/Input/PasswordInput.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const PasswordInput = ({value, onChange, placeholder}) => {
+
+const [isShowPassword, setIsShowPassword] = useState(false);
+const toggleShoWPassword = () => {
+  setIsShowPassword(!isShowPassword);
+}
   return (
-    <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3 outline-none'>
+    <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3 outline-0'>
       Password input
     </div>
   )
 }
 
-export default PasswordInput
+export default PasswordInput;
    
 
-// 14.59 resume
