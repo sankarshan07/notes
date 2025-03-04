0f96747cfc65a7943862608b33059fc28794a6ed
sankarshan <sankarshandesai3107@gmail.com>
2025-03-04 23:20:54 +0530

    searchbar implementaion

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+// import ProfileInfo from '../../components/Cards/ProfileInfo'
 
 const Home = () => {
   return (
-    <div >
-      Home 
-    </div>
+   <>
+   <Navbar/>
+  
+   </>
   )
 }
 
