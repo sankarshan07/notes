de44f486568003cf602dcd336aa8e06694b06697
sankarshan <sankarshandesai3107@gmail.com>
2025-03-04 21:16:04 +0530

    login and sigup setup complete

--- frontend/notes-app/src/components/Input/PasswordInput.jsx
+++ frontend/notes-app/src/components/Input/PasswordInput.jsx
@@ -1,18 +1,28 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
+import {FaRegEye, FaRegEyeSlash} from 'react-icons/fa6'
+const PasswordInput = ({ value, onChange, placeholder }) => {
+  const [isShowPassword, setIsShowPassword] = useState(false);
 
-const PasswordInput = ({value, onChange, placeholder}) => {
+  const toggleShowPassword = () => {
+    setIsShowPassword(!isShowPassword);
+  };
 
-const [isShowPassword, setIsShowPassword] = useState(false);
-const toggleShoWPassword = () => {
-  setIsShowPassword(!isShowPassword);
-}
   return (
-    <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3 outline-0'>
-      Password input
+    <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3 outline-none'>
+      <input
+        value={value}
+        onChange={onChange}
+        type={isShowPassword ? 'text' : 'password'}
+        placeholder={placeholder || 'Password'}
+        className='w-full text-sm bg-transparent py-3 mr-3 rounded outline-none'
+      />
+      {isShowPassword ? <FaRegEye type='button' size={22} className='text-blue-500' onClick={toggleShowPassword}>
+       
+      </FaRegEye>:<FaRegEyeSlash size={22}
+                   className='text-slate-400 cursor-pointer' onClick={() => toggleShowPassword()} 
+                   />}
     </div>
-  )
-}
+  );
+};
 
 export default PasswordInput;
-   
-
